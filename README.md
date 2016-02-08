eaebdd9d1f4bdea64fc053d1f35c84ff3ec8dea7
Zardwiz <sysadmin@watdo.org>
2016-02-07 19:35:03 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zip code and income cross-reference
+
+This is just something I threw together quickly because a client needed to exclude low-income zips from a PPC campaign.
+
+Data is from the 2000 census.
+
+Note that the ZCTA used by the census does NOT track perfectly with zip code boundaries! It's close enough for these purposes, though.
+
+Data accuracy is not guaranteed, and there is absolutely no warranty on this software.
---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+include("password_protect.php");
+?>
+<form method="post" action="index.php">
+2 Letter State Abbrev <input type="text" name="st">
+<input type="hidden" name="type" value="lowestmedianbystate">
+<br />
+Number of zips? <input type="text" name="num">
+Lowest income by state <input type="submit">
+</form>
+<hr />
+<form method="post" action="index.php">
+2 letter state abbrev <input type="text" name="st">
+<input type="hidden" name="type" value="highestpublicassistance">
+<br />
+Number of zips? <input type="text" name="num">;
+Highest % Public Assistance <input type="submit">
+</form>
+<hr />
+<form method="post" action="index.php">
+<input type="hidden" name="type" value="twodigit">
+<br />
+Number of zips? <input type="text" name="num"><br />
+First Digits Of Zip <input type="text" name="first"><br />
+Lowest Income By 2 or 3 Digit Zip <input type="submit"><br />
+Example: Lowest income near Grand Prairie, TX you could enter 750<br />
+	</form>
+	<hr />
+
+
+<?php
+global $state;
+$state = $_POST['st'];
+global $num;
+$num = $_POST['num'];
+if ($_POST['type'] = 'lowestmedianbystate'){
+	$db = new mysqli('localhost', 'root', '', 'ec');
+	if($db->connect_errno > 0){ echo $db->connect_error; }
+	$sql = "select * from censusdata where state = '$state' order by HH_w_earnings_mean_earnings ASC LIMIT $num";
+	$result = $db->query($sql);
+	echo "<center><b>Lowest Income Zip Codes</b></center>";
+	echo "<table border=1><tr>";
+	echo "<th>ID</th>";
+	echo "<th>Zip</th>";
+	echo "<th>HH Mean Earnings</th>";
+	echo "<th>HH W Pub Asst</th>";
+	echo "<th>HH Percent W Pub Asst</th>";
+	echo "<th>Per Capita - M > 15</th>";
+	echo "<th>Per Capita - F > 15</th>";
+	echo "<th>Per Capita</th>";
+	echo "</tr>";
+	while ($row = $result->fetch_assoc()){	
+	echo "<tr>";
+	$id = $row['id'];
+	echo "<td>" . $row['id'] . "</td>";	
+	echo "<td>" . $row['ZCTA'] . "</td>";
+	echo "<td>$" . $row['HH_w_earnings_mean_earnings'] . "</td>";
+	echo "<td>" . $row['HH_num_with_public_assistance_income'] . "</td>";
+	echo "<td>" . $row['HH_percent_with_public_assistance_income'] . "%</td>";
+	echo "<td>$" . $row['Males_over_15_median_per_capita_income'] . "</td>";
+	echo "<td>$" . $row['Females_over_15_median_per_capita_income'] . "</td>";
+	echo "<td>$" . $row['per_capita_income'] . "</td>";
+	echo "</tr>";
+	}	
+echo "</table>";
+}
+
+if ($_POST['type'] = 'highestpublicassistance'){
+	        $db = new mysqli('localhost', 'root', '', 'ec');
+		        if($db->connect_errno > 0){ echo $db->connect_error; }
+		        $sql = "select * from censusdata where state = '$state' order by HH_percent_with_public_assistance_income DESC LIMIT $num";
+		        $result = $db->query($sql);
+			        echo "<center><b>Zip Code With Highest % Public Assistance Income</b></center>";
+			        echo "<table border=1><tr>";
+				echo "<th>ID</th>";
+			        echo "<th>Zip</th>";
+			        echo "<th>HH Mean Earnings</th>";
+				echo "<th>HH W Pub Asst</th>";
+			        echo "<th>HH Percent W Pub Asst</th>";
+			        echo "<th>Per Capita - M > 15</th>";
+				echo "<th>Per Capita - F > 15</th>";
+			        echo "<th>Per Capita</th>";
+			        echo "</tr>";
+			        while ($row = $result->fetch_assoc()){
+				        echo "<tr>";
+				        $id = $row['id'];
+	 			        echo "<td>" . $row['id'] . "</td>";
+				        echo "<td>" . $row['ZCTA'] . "</td>";
+				        echo "<td>$" . $row['HH_w_earnings_mean_earnings'] . "</td>";
+				        echo "<td>" . $row['HH_num_with_public_assistance_income'] . "</td>";
+				        echo "<td>" . $row['HH_percent_with_public_assistance_income'] . "%</td>";
+				        echo "<td>$" . $row['Males_over_15_median_per_capita_income'] . "</td>";
+				        echo "<td>$" . $row['Females_over_15_median_per_capita_income'] . "</td>";
+				        echo "<td>$" . $row['per_capita_income'] . "</td>";
+				        echo "</tr>";
+				        }
+				echo "</table>";
+}
+
+if ($_POST['type'] = 'twodigit'){
+	        $db = new mysqli('localhost', 'root', '', 'ec');
+		if($db->connect_errno > 0){ echo $db->connect_error; }
+			$zcta = $_POST['first'];
+			$sql = "select * from censusdata where ZCTA LIKE '$zcta%' order by HH_w_earnings_mean_earnings ASC LIMIT $num";
+			echo $sql;
+		        $result = $db->query($sql);
+			echo "<center><b>Lowest Income By Partial Zip</b></center>";
+			echo "<table border=1><tr>";
+		        echo "<th>ID</th>";
+		        echo "<th>Zip</th>";
+		        echo "<th>HH Mean Earnings</th>";
+		        echo "<th>HH W Pub Asst</th>";
+		        echo "<th>HH Percent W Pub Asst</th>";
+	                echo "<th>Per Capita - M > 15</th>";
+		        echo "<th>Per Capita - F > 15</th>";
+		        echo "<th>Per Capita</th>";
+		        echo "</tr>";
+		        while ($row = $result->fetch_assoc()){
+			      echo "<tr>";
+			      $id = $row['id'];
+			      echo "<td>" . $row['id'] . "</td>";
+			      echo "<td>" . $row['ZCTA'] . "</td>";
+			      echo "<td>$" . $row['HH_w_earnings_mean_earnings'] . "</td>";
+			      echo "<td>" . $row['HH_num_with_public_assistance_income'] . "</td>";
+		              echo "<td>" . $row['HH_percent_with_public_assistance_income'] . "%</td>";
+		              echo "<td>$" . $row['Males_over_15_median_per_capita_income'] . "</td>";
+			      echo "<td>$" . $row['Females_over_15_median_per_capita_income'] . "</td>";
+      		              echo "<td>$" . $row['per_capita_income'] . "</td>"; 
+			      echo "</tr>";
+				}
+			echo "</table>";
+}
+
+?>
