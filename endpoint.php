a5cda520038eff0ca23b7481b6ce652db2bf12df
Zardwiz <sysadmin@watdo.org>
2016-02-07 22:38:01 -0500

    Initial. Nothing here yet.

---
+++ endpoint.php
@@ -0,0 +1,5 @@
+<?php
+// API endpoint
+// ?state=VT&county=Chittenden&minassistance=5&maxassistance=10&minhhinc=1000&maxhhinc=10000
+// Everything gets wrapped into the query - if present, search on it. If not, ignore.
+?>
