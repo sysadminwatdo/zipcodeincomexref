fa3facdf1af63670ae64501ca6891e1f1c03ce8c
Zardwiz <sysadmin@watdo.org>
2016-02-08 15:50:12 -0500

    File

--- pw-example.sh
+++ pw-example.sh
@@ -1,3 +1,4 @@
 sed -i "s/PasswordHere/RealPWGoesHere/g" "index.php"
 sed -i "s/PasswordHere/RealPWGoesHere/g" "fixstate.php"
 sed -i "s/PasswordHere/RealPWGoesHere/g" "export.php"
+sed -i "s/PasswordHere/RealPWGoesHere/g" "exportstate.php"
