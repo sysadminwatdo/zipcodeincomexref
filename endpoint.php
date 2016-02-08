9fbc530b235232c4b4df40066498a864b2a2e880
Zardwiz <sysadmin@watdo.org>
2016-02-08 11:27:23 -0500

    Fleshing out how this will work

--- endpoint.php
+++ endpoint.php
@@ -2,4 +2,18 @@
 // API endpoint
 // ?state=VT&county=Chittenden&minassistance=5&maxassistance=10&minhhinc=1000&maxhhinc=10000
 // Everything gets wrapped into the query - if present, search on it. If not, ignore.
+// should return either xml or csv ?format=xml|csv
+// state=XX
+// county (NoSpaces)(PrinceGeorges)
+// town (NoSpaces)NewBloomfield
+// zip (5 digits)
+// minassistance (digits only)
+// maxassistance (digits only)
+// minhhinc (digits only)
+// maxhhinc (digits only)
+// minmalepercap (digits only)
+// maxmalepercap (digits only) 
+// minfemalepercap (digits only)
+// maxfemalepercap (digits only)
+//
 ?>
