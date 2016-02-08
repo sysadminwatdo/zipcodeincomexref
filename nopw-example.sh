3c1a207d2db195416ab482327a883f820366f6a5
Zardwiz <sysadmin@watdo.org>
2016-02-08 15:50:04 -0500

    File

--- nopw-example.sh
+++ nopw-example.sh
@@ -1,3 +1,4 @@
 sed -i "s/RealPWGoesHere/PasswordHere/g" "index.php"
 sed -i "s/ReadPWGoesHere/PasswordHere/g" "fixstate.php"
 sed -i "s/RealPWGoesHere/PasswordHere/g" "export.php"
+sed -i "s/RealPWGoesHere/PasswordHere/g" "exportstate.php"
