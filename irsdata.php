4f339d9312d328109c4aba2fc65f2a63a6f8e0e2
Zardwiz <sysadmin@watdo.org>
2016-02-08 23:34:31 -0500

    Initial

---
+++ irsdata.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+This will be a complicated select.
+By State
+By zip stub
+By income upper/lower bounds
+By married returns vs single returns
+
+Present human-friendly names to user, select by col names
+
+Add cols for relative percentages MFJ MFS etc - simple select instead of math on 150k rows
+
+() AGI Lower ________
+() AGI Upper ________
+() % MFJ     ________
+() % MFS     ________
+() Num Dep Upper _____
+() Num Dep Lower ____
+etc.
